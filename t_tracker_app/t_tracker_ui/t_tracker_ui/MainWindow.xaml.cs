2e5982d974866db08255d041d74064cbab3d3911
Birtalan Tibor József <171870151+bbitibb@users.noreply.github.com>
2025-09-12 14:33:30 +0200

    dev

--- t_tracker_app/t_tracker_ui/t_tracker_ui/MainWindow.xaml.cs
+++ t_tracker_app/t_tracker_ui/t_tracker_ui/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
+using Windows.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using t_tracker_ui.Views;
-
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
 namespace t_tracker_ui;
 
 public sealed partial class MainWindow : Window
@@ -11,6 +13,13 @@ public sealed partial class MainWindow : Window
         
         InitializeComponent();
         
+        AppWindow.Resize(new Windows.Graphics.SizeInt32(800, 500));
+
+        AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+
+        if (Content is FrameworkElement root)
+            root.ActualThemeChanged += (_, __) => ApplyTitleBarTheme();
+
         this.Activated += OnFirstActivated;
     }
     private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
@@ -39,5 +48,30 @@ public sealed partial class MainWindow : Window
 
         if (ContentFrame.CurrentSourcePageType != typeof(DashboardPage))
             ContentFrame.Navigate(typeof(DashboardPage));
+    }
+    private void ApplyTitleBarTheme()
+    {
+        var appWindow = this.AppWindow;
+        if (!AppWindowTitleBar.IsCustomizationSupported())
+            return;
+
+        var titleBar = appWindow.TitleBar;
+
+        var isDark = (Content as FrameworkElement)?.ActualTheme == ElementTheme.Dark;
+
+        Color bg = isDark ? Color.FromArgb(255, 32, 32, 32) : Color.FromArgb(255, 243, 243, 243);
+        Color fg = isDark ? Colors.White : Colors.Black;
+
+        titleBar.ExtendsContentIntoTitleBar = false;
+
+        titleBar.BackgroundColor                 = bg;
+        titleBar.ForegroundColor                 = fg;
+        titleBar.InactiveBackgroundColor         = bg;
+        titleBar.InactiveForegroundColor         = fg;
+
+        titleBar.ButtonBackgroundColor           = bg;
+        titleBar.ButtonForegroundColor           = fg;
+        titleBar.ButtonInactiveBackgroundColor   = bg;
+        titleBar.ButtonInactiveForegroundColor   = fg;
     }
 }
