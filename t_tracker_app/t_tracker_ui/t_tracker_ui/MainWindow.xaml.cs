6d8b65749a4e03f52b98e9d5ca3dc7ee12bca91c
Birtalan Tibor József <171870151+bbitibb@users.noreply.github.com>
2025-09-12 16:13:19 +0200

    windows sdk version upgrade

--- t_tracker_app/t_tracker_ui/t_tracker_ui/MainWindow.xaml.cs
+++ t_tracker_app/t_tracker_ui/t_tracker_ui/MainWindow.xaml.cs
@@ -1,26 +1,42 @@
+using System.Linq;
 using Windows.UI;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using t_tracker_ui.Views;
 using Microsoft.UI.Windowing;
-using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+
 namespace t_tracker_ui;
 
 public sealed partial class MainWindow : Window
 {
+    private AppWindow _appWindow;
     public MainWindow()
     {
-        
         InitializeComponent();
-        
-        AppWindow.Resize(new Windows.Graphics.SizeInt32(800, 500));
 
-        AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+        SystemBackdrop = new MicaBackdrop();
+
+        _appWindow = AppWindow;
+        var tb = _appWindow.TitleBar;
+        tb.ExtendsContentIntoTitleBar = true;
+
+        tb.BackgroundColor = Colors.Transparent;
+        tb.InactiveBackgroundColor = Colors.Transparent;
+        tb.ButtonBackgroundColor = Colors.Transparent;
+        tb.ButtonInactiveBackgroundColor = Colors.Transparent;
+        ApplyTitleBarTheme();
+
+        SetTitleBar(AppTitleBar);
+
+        _appWindow.Changed += OnAppWindowChanged;
+        UpdateTitleBarLayout();
+
+        Activated += OnFirstActivated;
 
         if (Content is FrameworkElement root)
             root.ActualThemeChanged += (_, __) => ApplyTitleBarTheme();
-
-        this.Activated += OnFirstActivated;
     }
     private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
     {
@@ -41,37 +57,40 @@ public sealed partial class MainWindow : Window
     }
     private void OnFirstActivated(object sender, WindowActivatedEventArgs e)
     {
-        this.Activated -= OnFirstActivated;
+        Activated -= OnFirstActivated;
 
-        if (NavView.MenuItems.Count > 0)
-            NavView.SelectedItem = NavView.MenuItems[0];
+        foreach (var mi in NavView.MenuItems)
+        {
+            if (mi is NavigationViewItem nvi && (string)nvi.Tag == "StatisticsPage")
+            {
+                NavView.SelectedItem = nvi;
+                break;
+            }
+        }
 
         if (ContentFrame.CurrentSourcePageType != typeof(DashboardPage))
             ContentFrame.Navigate(typeof(DashboardPage));
     }
+    private void UpdateTitleBarLayout()
+    {
+        var tb = _appWindow.TitleBar;
+        AppTitleBar.Height = tb.Height;
+        LeftPaddingCol.Width  = new GridLength(tb.LeftInset);
+        RightPaddingCol.Width = new GridLength(tb.RightInset);
+    }
     private void ApplyTitleBarTheme()
     {
-        var appWindow = this.AppWindow;
-        if (!AppWindowTitleBar.IsCustomizationSupported())
-            return;
-
-        var titleBar = appWindow.TitleBar;
-
         var isDark = (Content as FrameworkElement)?.ActualTheme == ElementTheme.Dark;
+        var fg = isDark ? Colors.White : Colors.Black;
 
-        Color bg = isDark ? Color.FromArgb(255, 32, 32, 32) : Color.FromArgb(255, 243, 243, 243);
-        Color fg = isDark ? Colors.White : Colors.Black;
-
-        titleBar.ExtendsContentIntoTitleBar = false;
-
-        titleBar.BackgroundColor                 = bg;
-        titleBar.ForegroundColor                 = fg;
-        titleBar.InactiveBackgroundColor         = bg;
-        titleBar.InactiveForegroundColor         = fg;
-
-        titleBar.ButtonBackgroundColor           = bg;
-        titleBar.ButtonForegroundColor           = fg;
-        titleBar.ButtonInactiveBackgroundColor   = bg;
-        titleBar.ButtonInactiveForegroundColor   = fg;
+        var tb = _appWindow.TitleBar;
+        tb.ForegroundColor = fg;
+        tb.InactiveForegroundColor = fg;
+        tb.ButtonForegroundColor = fg;
+        tb.ButtonInactiveForegroundColor = fg;
+    }
+    private void OnAppWindowChanged(AppWindow sender, AppWindowChangedEventArgs args)
+    {
+        DispatcherQueue.TryEnqueue(UpdateTitleBarLayout);
     }
 }
