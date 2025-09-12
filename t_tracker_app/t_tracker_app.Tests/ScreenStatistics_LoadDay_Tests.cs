1a12ec134c14fe2478197309c3ef26595da8ecc1
Birtalan Tibor József <171870151+bbitibb@users.noreply.github.com>
2025-09-12 14:34:43 +0200

    dev

--- t_tracker_app/t_tracker_app.Tests/ScreenStatistics_LoadDay_Tests.cs
+++ t_tracker_app/t_tracker_app.Tests/ScreenStatistics_LoadDay_Tests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using t_tracker_app.core;
 using t_tracker_app;
 using Xunit;
 
--- t_tracker_app/t_tracker_app.core/ScreenStatistics.cs
+++ t_tracker_app/t_tracker_app.core/ScreenStatistics.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
-namespace t_tracker_app;
+namespace t_tracker_app.core;
 
 public class ScreenStatistics
 {
